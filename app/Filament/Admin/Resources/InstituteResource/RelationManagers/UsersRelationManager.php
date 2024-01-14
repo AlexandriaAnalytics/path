aaa3e452ce09f9ecbdebf5934ffb1a3cddfdba64
Matias Yocca <matias@yocca.dev>
2024-01-14 20:22:42 +0000

    customize institute users table

--- app/Filament/Admin/Resources/InstituteResource/RelationManagers/UsersRelationManager.php
+++ app/Filament/Admin/Resources/InstituteResource/RelationManagers/UsersRelationManager.php
@@ -3,13 +3,10 @@
 namespace App\Filament\Admin\Resources\InstituteResource\RelationManagers;
 
 use App\Filament\Admin\Resources\UserResource;
-use Filament\Forms;
 use Filament\Forms\Form;
 use Filament\Resources\RelationManagers\RelationManager;
-use Filament\Tables;
 use Filament\Tables\Table;
-use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Illuminate\Database\Eloquent\Model;
 
 class UsersRelationManager extends RelationManager
 {
@@ -22,6 +19,11 @@ class UsersRelationManager extends RelationManager
 
     public function table(Table $table): Table
     {
-        return UserResource::table($table);
+        return UserResource::table($table)
+            ->heading('Authorised Users')
+            ->description('Users authorised to access this institute via the management portal')
+            ->recordUrl(
+                fn (Model $record): string => UserResource::getUrl('view', ['record' => $record]),
+            );
     }
 }
