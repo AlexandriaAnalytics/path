64d140eca16a0bb58638b1a09c829396ed763b53
fedejsegura <fedej.segura@gmail.com>
2024-01-13 21:07:31 -0300

    migraciones skillsy levels

---
+++ database/migrations/2024_01_14_000417_create_levels_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLevelsTable extends Migration
+{
+    /**
+     * * Run the migrations.
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('levels', function (Blueprint $table) {
+            $table->increments('id_level')->unique();
+            $table->string('name');
+            $table->double('price');
+            $table->string('slug');
+            $table->text('modules')->nullable();
+            $table->timestamps();
+            $table->unsignedInteger('tier')->nullable();
+            $table->double('complete_price');
+        });
+    }
+
+    /**
+     * * Reverse the migrations.
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('levels');
+    }
+}
