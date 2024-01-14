c32a5a6cd46a28722e4b271a4362c25b29e87d4d
fedejsegura <fedej.segura@gmail.com>
2024-01-13 21:14:48 -0300

    modelo AvailableSkill

--- app/Models/AvailableSkill.php
+++ app/Models/AvailableSkill.php
@@ -2,10 +2,42 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Exam;
+use App\Models\Skills;
 use Illuminate\Database\Eloquent\Model;
 
 class AvailableSkill extends Model
 {
-    use HasFactory;
+    /**
+     * * User primary key.
+     * @var string
+     */
+    protected $primaryKey = 'id_available_skill';
+
+    /**
+     * * The attributes that are mass assignable.
+     * @var array
+     */
+    protected $fillable = [
+        'id_exam',
+        'id_skill',
+    ];
+
+    /**
+     * * Get the Exam that owns the AvailableLevel
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function exam()
+    {
+        return $this->belongsTo(Exam::class, 'id_exam', 'id_exam');
+    }
+
+    /**
+     * * Get the Level that owns the AvailableLevel
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function skill()
+    {
+        return $this->belongsTo(Skills::class, 'id_skill', 'id_skill');
+    }
 }
