99d8eb15e723f3dd270ba2528c62466747dc7799
fedejsegura <fedej.segura@gmail.com>
2024-01-13 21:14:23 -0300

    modelo AvailableLevel

--- app/Models/AvailableLevel.php
+++ app/Models/AvailableLevel.php
@@ -2,10 +2,42 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Exam;
+use App\Models\Level;
 use Illuminate\Database\Eloquent\Model;
 
 class AvailableLevel extends Model
 {
-    use HasFactory;
+    /**
+     * * User primary key.
+     * @var string
+     */
+    protected $primaryKey = 'id_available_level';
+
+    /**
+     * * The attributes that are mass assignable.
+     * @var array
+     */
+    protected $fillable = [
+        'id_exam',
+        'id_level',
+    ];
+
+    /**
+     * * Get the Exam that owns the AvailableLevel
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function exam()
+    {
+        return $this->belongsTo(Exam::class, 'id_exam', 'id_exam');
+    }
+
+    /**
+     * * Get the Level that owns the AvailableLevel
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function level()
+    {
+        return $this->belongsTo(Level::class, 'id_level', 'id_level');
+    }
 }
