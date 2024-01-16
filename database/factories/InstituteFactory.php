40afd0a138037199d9935513b1e160d14fb32f50
Matias Yocca <matias@yocca.dev>
2024-01-16 02:35:57 +0000

    fix institute factory

--- database/factories/InstituteFactory.php
+++ database/factories/InstituteFactory.php
@@ -17,7 +17,8 @@ class InstituteFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->company,
+            'type' => fake()->randomElement(\App\Enums\InstituteType::values()),
+            'name' => fake()->company(),
         ];
     }
 }
