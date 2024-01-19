8d8b5f647f4c281424a1f3f8663450a744fb389e
Matias Yocca <matias@yocca.dev>
2024-01-19 04:55:28 +0000

    modify institute relation operations

--- app/Filament/Admin/Resources/InstituteResource/RelationManagers/UsersRelationManager.php
+++ app/Filament/Admin/Resources/InstituteResource/RelationManagers/UsersRelationManager.php
@@ -5,6 +5,7 @@ namespace App\Filament\Admin\Resources\InstituteResource\RelationManagers;
 use App\Filament\Admin\Resources\UserResource;
 use Filament\Forms\Form;
 use Filament\Resources\RelationManagers\RelationManager;
+use Filament\Tables;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Model;
 
@@ -20,10 +21,20 @@ class UsersRelationManager extends RelationManager
     public function table(Table $table): Table
     {
         return UserResource::table($table)
+            ->recordTitleAttribute('name')
             ->heading('Authorised Users')
             ->description('Users authorised to access this institute via the management portal')
             ->recordUrl(
                 fn (Model $record): string => UserResource::getUrl('view', ['record' => $record]),
-            );
+            )
+            ->headerActions([
+                Tables\Actions\AttachAction::make()
+                    ->recordSelectSearchColumns(['name', 'email'])
+            ])
+            ->actions([
+                Tables\Actions\DetachAction::make(),
+            ])
+            ->paginated([5, 10, 25])
+            ->defaultPaginationPageOption(5);
     }
 }
