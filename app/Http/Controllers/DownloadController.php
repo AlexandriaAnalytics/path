7102768e3fa94eea3a2c0310231fac0d15e292b3
Rodrigo Pavón Gómez <rodrigopavongomez@gmail.com>
2024-01-19 22:07:16 +0000

    delete comments

--- app/Http/Controllers/DownloadController.php
+++ app/Http/Controllers/DownloadController.php
@@ -6,8 +6,8 @@ use PDF;
 
 class DownloadController extends Controller {
     public function downloadCandidate() {
-    $data = []; // Pass any data needed for the Blade template
-    $html = view('example', $data)->render(); // Render the Blade template
+    $data = []; 
+    $html = view('example', $data)->render(); 
 
     $pdf = PDF::loadHTML($html);
     return $pdf->download('downloaded_pdf.pdf');
