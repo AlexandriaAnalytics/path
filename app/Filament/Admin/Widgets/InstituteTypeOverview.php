b6ba357ee6c0ffe7de61d878db4435898c668fc7
fedejsegura <fedej.segura@gmail.com>
2024-01-21 11:28:14 -0300

    cards en el dashboard

---
+++ app/Filament/Admin/Widgets/InstituteTypeOverview.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Filament\Admin\Widgets;
+
+use App\Models\Exam;
+use Filament\Widgets\StatsOverviewWidget as BaseWidget;
+use Filament\Widgets\StatsOverviewWidget\Stat;
+use App\Models\Institute;
+use App\Models\Student;
+
+class InstituteTypeOverview extends BaseWidget
+{
+    protected function getStats(): array
+    {
+        return [
+            Stat::make('Institutes', Institute::all()->count()),
+            Stat::make('Exams', Exam::all()->count()),
+            Stat::make('Students', Student::all()->count())
+        ];
+    }
+}
