7ae6daf0dfa042d32dbb1ab294942db0eab3a8bf
Matias Yocca <matias@yocca.dev>
2024-01-22 21:16:52 +0000

    fix dependencies and missing column

--- database/migrations/2024_01_09_185149_create_institutes_table.php
+++ database/migrations/2024_01_09_185149_create_institutes_table.php
@@ -16,7 +16,8 @@ return new class extends Migration
 
             $table->string('name')->nullable();
             $table->string('files_url')->nullable();
-            
+            $table->boolean('can_add_candidates');
+
             $table->foreignId('institute_type_id')->constrained('institute_types')->cascadeOnDelete();
             $table->foreignId('owner_id')->nullable()->constrained('users')->cascadeOnDelete();
 
