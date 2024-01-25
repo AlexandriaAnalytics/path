51e6b5f5fdc968265fd9d341c8263769835c7d11
fedejsegura <fedej.segura@gmail.com>
2024-01-25 16:14:49 -0300

    exam_module

--- database/migrations/2024_01_25_181546_create_modules_table.php
+++ database/migrations/2024_01_25_181546_create_modules_table.php
@@ -4,26 +4,46 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class CreateModulesTable extends Migration
 {
     /**
-     * Run the migrations.
+     * * Run the migrations.
+     * @return void
      */
-    public function up(): void
+    public function up()
     {
         Schema::create('modules', function (Blueprint $table) {
             $table->id();
+
             $table->string('name');
-            $table->unsignedInteger('price');
+            $table->decimal('price', 12, 2);
+
+            $table->timestamps();
+            $table->softDeletes();
+        });
+
+        Schema::create('exam_module', function (Blueprint $table) {
+            $table->id();
+
+            $table->foreignId('exam_id')
+                ->constrained('exams')
+                ->cascadeOnDelete();
+
+            $table->foreignId('module_id')
+                ->constrained('modules')
+                ->cascadeOnDelete();
+
             $table->timestamps();
         });
     }
 
     /**
-     * Reverse the migrations.
+     * * Reverse the migrations.
+     * @return void
      */
-    public function down(): void
+    public function down()
     {
+        Schema::dropIfExists('exam_module');
         Schema::dropIfExists('modules');
     }
-};
+}
