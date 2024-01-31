0df992e3f97c702b14f69b355f265708a5220a4f
fedejsegura <fedej.segura@gmail.com>
2024-01-31 19:44:22 +0000

    abrir enlace a wpp

--- app/Filament/Admin/Resources/InstituteResource.php
+++ app/Filament/Admin/Resources/InstituteResource.php
@@ -135,6 +135,7 @@ class InstituteResource extends Resource
                     ->sortable()
                     ->toggleable(isToggledHiddenByDefault: true),
                 Tables\Columns\TextColumn::make('phone')
+                    ->url(fn ($record) => 'https://api.whatsapp.com/send?phone=' . $record->phone, shouldOpenInNewTab: true)
                     ->searchable()
                     ->sortable()
                     ->toggleable(isToggledHiddenByDefault: true),
