064d93be6332d4b2458610a63b185f7af20d7fd4
Andres Fernandez <andres.fernandezcaballero@davinci.edu.ar>
2024-02-19 18:42:21 +0000

    ADD mp hook to white liste of crf token list

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -13,6 +13,6 @@ class VerifyCsrfToken extends Middleware
      */
     protected $except = [
         '/payment/webhook/paypal',
-        '/payment/webhook/mercadopago',
+        '/payment/webhook/mp',
     ];
 }
