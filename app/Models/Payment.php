2e92856ce252291daf9945aa84e1393f7799b0e7
Andres Fernandez <andres.fernandezcaballero@davinci.edu.ar>
2024-02-19 05:12:18 +0000

    add payment resource and migration

---
+++ app/Models/Payment.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Payment extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'payment_method',
+        'payment_id',
+        'currency',
+        'amount',
+        'status',
+        'instalment_number',
+        'candidate_id'
+    ];
+}
