dea7c54d2f4217a771ac415cb1660313b2ef3117
Andres Fernandez <andres.fernandezcaballero@davinci.edu.ar>
2024-02-27 12:41:51 +0000

    Add payment by institutes

---
+++ app/Filament/Resources/FinancingResource/Pages/CreateFinancing.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Filament\Resources\FinancingResource\Pages;
+
+use App\Filament\Resources\FinancingResource;
+use Filament\Actions;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateFinancing extends CreateRecord
+{
+    protected static string $resource = FinancingResource::class;
+}
