90e6f82f1e21c7973c97277db6153f875a3f9dd5
Rodrigo Pavon <rodrigopavongomez@gmail.com>
2024-02-27 03:02:34 +0000

    add changes

--- app/Filament/Exports/CandidateExporter.php
+++ app/Filament/Exports/CandidateExporter.php
@@ -27,9 +27,11 @@ class CandidateExporter extends Exporter
             ExportColumn::make('status'),
             ExportColumn::make('type_of_certificate')
                 ->label('Type of Certificate'),
+
             ExportColumn::make('student.institute.name')->label('Institute Name'),
             ExportColumn::make('modules.name')->label('Module Name'),
             ExportColumn::make('pendingModules.name')->label('Pending Modules'),
+
         ];
     }
     
