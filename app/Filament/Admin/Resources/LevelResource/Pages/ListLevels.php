7d2e99466330e8f49ba0b7bff20a1bcffcdcfadd
fedejsegura <fedej.segura@gmail.com>
2024-03-02 23:38:42 +0000

    boton new exam

--- app/Filament/Admin/Resources/LevelResource/Pages/ListLevels.php
+++ app/Filament/Admin/Resources/LevelResource/Pages/ListLevels.php
@@ -13,7 +13,8 @@ class ListLevels extends ListRecords
     protected function getHeaderActions(): array
     {
         return [
-            Actions\CreateAction::make(),
+            Actions\CreateAction::make()
+                ->label('New exam'),
         ];
     }
 }
