e77d3f6e23ef2339fafb9914e1ae0568d3256111
Matias Yocca <matias@yocca.dev>
2024-03-04 03:08:02 +0000

    add notifications for each member type

---
+++ app/Filament/Admin/Resources/NotificationResource/Pages/ViewNotification.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Filament\Admin\Resources\NotificationResource\Pages;
+
+use App\Filament\Admin\Resources\NotificationResource;
+use Filament\Actions;
+use Filament\Resources\Pages\ViewRecord;
+
+class ViewNotification extends ViewRecord
+{
+    protected static string $resource = NotificationResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            //
+        ];
+    }
+}
