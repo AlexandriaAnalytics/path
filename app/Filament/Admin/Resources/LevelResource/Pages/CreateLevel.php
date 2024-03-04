839de3a44084b1920ea052eeb2f800acf6d237c0
camilaarce <camilarce2710@gmail.com>
2024-03-04 19:07:53 +0000

    title create exam

--- app/Filament/Admin/Resources/LevelResource/Pages/CreateLevel.php
+++ app/Filament/Admin/Resources/LevelResource/Pages/CreateLevel.php
@@ -5,10 +5,16 @@ namespace App\Filament\Admin\Resources\LevelResource\Pages;
 use App\Filament\Admin\Resources\LevelResource;
 use Filament\Actions;
 use Filament\Resources\Pages\CreateRecord;
+use Illuminate\Contracts\Support\Htmlable;
 
 class CreateLevel extends CreateRecord
 {
     protected static string $resource = LevelResource::class;
+
+    public function getTitle(): string | Htmlable
+    {
+        return __('Create exam');
+    }
 
     protected function getRedirectUrl(): string
     {
