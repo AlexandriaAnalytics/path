8d93131c58bdbda884dec87b00a9b2b8c0497c9e
camilaarce <camilarce2710@gmail.com>
2024-03-08 18:37:43 +0000

    fix dashboard candidates

--- app/Filament/Candidate/Resources/CandidateResource/Widgets/ExamSession.php
+++ app/Filament/Candidate/Resources/CandidateResource/Widgets/ExamSession.php
@@ -8,6 +8,7 @@ use Filament\Tables;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
 use Filament\Widgets\TableWidget as BaseWidget;
+use Illuminate\Contracts\Support\Htmlable;
 
 class ExamSession extends BaseWidget
 {
@@ -15,17 +16,18 @@ class ExamSession extends BaseWidget
     {
         return $table
             ->query(CandidateExam::query())
+            ->heading('Exam session')
             ->columns([
                 TextColumn::make('exam.session_name')
-                ->label('Session name'),
+                    ->label('Session name'),
                 TextColumn::make('module.name'),
                 TextColumn::make('exam.type')
-                ->label('type'),
+                    ->label('Type'),
                 TextColumn::make('exam.scheduled_date')
-                ->label('Scheduled date')
-                ->date('d-m-Y h:m'),
+                    ->label('Scheduled date')
+                    ->date('d-m-Y h:m'),
                 TextColumn::make('exam.location')
-                ->label('Location')
+                    ->label('Location')
             ]);
     }
 }
