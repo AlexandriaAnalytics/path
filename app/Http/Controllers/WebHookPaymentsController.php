adae7028f97dd8953fe7c3d286bc26623f8b9f7e
Matias Yocca <matias@yocca.dev>
2024-03-08 23:20:08 +0000

    handle mercadopago subscription notification

--- app/Http/Controllers/WebHookPaymentsController.php
+++ app/Http/Controllers/WebHookPaymentsController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use App\Models\Candidate;
 use App\Models\Payment;
 use App\Services\Payment\StripePaymentMethod;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Log;
 
@@ -75,12 +76,12 @@ class WebHookPaymentsController extends Controller
     public function mercadopagoWebhook(Request $request)
     {
         $type = $request->string('type')->toString();
-        Log::info('webhook-mp', $request->all());
+
         return match ($type) {
             'payment' => $this->handleMercadoPagoPayment($request),
-            'subscription_preapproval',  'subscription_authorized_payment' =>
-            $this->handleMercadoPagoSubscription($request),
-            default => response()->json(['status' => 'not_implemented'], 501),
+            'subscription_preapproval' => $this->handleMercadoPagoSubscription($request),
+            'subscription_authorized_payment' => $this->handleMercadoPagoSubscriptionPayment($request),
+            default => $this->notImplementedResponse($request),
         };
     }
 
@@ -122,19 +123,66 @@ class WebHookPaymentsController extends Controller
 
     private function handleMercadoPagoSubscription(Request $request)
     {
-        $subscriptionId = $request->input('data.id');
+        // $subscriptionId = $request->input('data.id');
 
-        $token = config('mercadopago.access_token');
+        // $token = config('mercadopago.access_token');
 
-        $url = 'https://api.mercadopago.com/preapproval/' . $subscriptionId;
+        // $url = 'https://api.mercadopago.com/preapproval/' . $subscriptionId;
+
+        // $response = Http::withToken($token)
+        //     ->get($url);
+
+        // if ($response->failed()) {
+        //     return response()->json(['status' => 'error'], 500);
+        // }
+
+        return response()->json(['status' => 'not_implemented'], 500);
+    }
+
+    private function handleMercadoPagoSubscriptionPayment(Request $request)
+    {
+        $paymentId = $request->input('data.id');
+
+        $token = config('mercadopago.access_token.ARG');
 
         $response = Http::withToken($token)
-            ->get($url);
+            ->get("https://api.mercadopago.com/authorized_payments/{$paymentId}");
 
         if ($response->failed()) {
+            report(new \Exception('Webhook error' . $request->json()));
             return response()->json(['status' => 'error'], 500);
         }
 
-        return response()->json(['status' => 'not_implemented'], 500);
+        $response = Http::withToken($token)->get(
+            "https://api.mercadopago.com/preapproval/" .
+                $response->json("preapproval_id")
+        );
+
+        if ($response->failed()) {
+            report(new \Exception('Webhook error' . $request->json()));
+            return response()->json(['status' => 'error'], 500);
+        }
+
+        Candidate::findOrFail($response->json('external_reference'))
+            ->payments()
+            ->where('current_instalment', $response->json('summarized.charged_quantity'))
+            ->update([
+                'status' => 'approved',
+                'payment_id' => $paymentId,
+                'paid_date' => $response->json('summarized.last_charged_date')
+            ]);
+
+        return $this->successResponse();
+    }
+
+    private function successResponse()
+    {
+        return response()->json(['status' => 'success']);
+    }
+
+    private function notImplementedResponse(Request $request)
+    {
+        report(new \Exception('Webhook not implemented' . $request->json()));
+        return response()->json(['status' => 'not_implemented'], 501);
     }
 }
