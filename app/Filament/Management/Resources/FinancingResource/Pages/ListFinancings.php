1de17543e8179363b84c07d3835f9ba686042b23
camilaarce <camilarce2710@gmail.com>
2024-03-12 18:31:23 +0000

    send payment

--- app/Filament/Management/Resources/FinancingResource/Pages/ListFinancings.php
+++ app/Filament/Management/Resources/FinancingResource/Pages/ListFinancings.php
@@ -3,15 +3,19 @@
 namespace App\Filament\Management\Resources\FinancingResource\Pages;
 
 use App\Enums\PaymentMethod;
+use App\Enums\StatusEnum;
 use App\Enums\UserStatus;
 use App\Filament\Management\Resources\FinancingResource;
 use App\Filament\Management\Widgets\FinancingPaidWidget;
 use App\Filament\Management\Widgets\FinancingUnpaidWidget;
 use App\Filament\Management\Widgets\FinancingWidget;
+use App\Models\Candidate;
+use App\Models\Country;
 use App\Models\Financing;
 use App\Models\Institute;
 use App\Models\InstitutePayment;
 use App\Models\Payment;
+use App\Models\Student;
 use Carbon\Carbon;
 use Cmgmyr\PHPLOC\Log\Text;
 use Filament\Actions;
@@ -54,50 +58,88 @@ class ListFinancings extends ListRecords
                 ->label('Send payment')
                 ->form([
                     TextInput::make('amount')
-                        ->prefix(fn () => Filament::getTenant()->currency)
-                        ->default(function () {
-                            $financings = Filament::getTenant()->financings;
-                            $totalGroupAmount = 0;
-                            foreach ($financings as $financing) {
-                                $totalGroupAmount += floatval($financing
-                                    ->payments()
-                                    ->orderBy('current_installment', 'ASC')
-                                    ->first()->amount);
-                            }
-                            return $totalGroupAmount;
-                        })
+                        ->numeric()
                         ->readOnly(),
-                    TextInput::make('payment_id')->readOnly()
+
+                    Select::make('candidate_id')
+                        ->label('Candidate')
+                        ->placeholder('Select a candidate')
+                        ->required()
+                        ->searchable()
+                        ->preload()
+                        ->live()
+                        ->reactive()
+                        ->relationship('candidate')
+                        ->multiple()
+                        ->options(function () {
+                            $instituteId = Filament::getTenant()->id;
+
+                            if (!$instituteId) {
+                                return [];
+                            }
+
+                            $candidates = Candidate::query()->whereHas('student.institute', function ($query) use ($instituteId) {
+                                $query->where('id', $instituteId);
+                            })->get();
+
+                            $students = [];
+
+                            foreach ($candidates as $candidate) {
+                                $students[] .= "{$candidate->student->name} {$candidate->student->surname}";
+                            }
+
+                            return $students;
+                        })
+                        ->getOptionLabelFromRecordUsing(fn (Student $record) => "{$record->name} {$record->surname}")
+                        ->afterStateUpdated(function (Set $set) {
+                            foreach ($this->mountedActionsData[0]['candidate_id'] as $candidate) {
+                                $concepts = Candidate::find($candidate + 1)->concepts;
+                                $totalAmount = 0;
+                                foreach ($concepts as $concept) {
+                                    if ($concept->type->value == 'exam' || $concept->type->value == 'module') {
+                                        $totalAmount = $totalAmount + $concept->amount;
+                                    }
+                                    if ($concept->type->value == 'registration_fee' && Institute::find(Filament::getTenant()->id)->can_view_registration_fee == 1) {
+                                        $totalAmount = $totalAmount - $concept->amount;
+                                    }
+                                }
+                            }
+                            $set('amount', $totalAmount);
+                        }),
+
+                    TextInput::make('payment_id')
                         ->label('Payment ID')
-                        ->default(fn () => 'd' . Carbon::now()->timestamp . rand(1000, 9000))->readOnly(),
+                        ->default(fn () => 'd' . Carbon::now()->timestamp . rand(1000, 9000))
+                        ->readOnly(),
                     TextInput::make('link_to_ticket')
                         ->required(),
-
                     MarkdownEditor::make('description')
-                        ->required()
                 ])
                 ->action(function (array $data) {
-                    $payment = Payment::create([
-                        'institute_id' => Filament::getTenant()->id,
-                        'amount' => $data['amount'],
-                        'status' => UserStatus::Processing_payment->value,
-                        'payment_method' => PaymentMethod::TRANSFER->value,
-                        'payment_id' => $data['payment_id'],
-                        'currency' => Filament::getTenant()->currency,
-                        'current_period' => Carbon::now()->day(1),
-                        'link_to_ticket' => $data['link_to_ticket'],
-                        'description' => $data['description'],
-                    ]);
-
-                    $financings = Filament::getTenant()->financings
-                        ->each(fn ($item) => $item->update(['state' => 'stack']));
-
-                    $financings = Filament::getTenant()->financings->each(fn ($item) => $item->payments->sortBy('current_installment', 1)->first()->update(['pay_id' => $payment->id]));
-
-                    Notification::make('payment_created')
-                        ->title('Payment created successfuly')
-                        ->color('success')
-                        ->send();
+                    foreach ($this->mountedActionsData[0]['candidate_id'] as $candidate) {
+                        $newPayment = new Payment();
+                        $newPayment->institute_id = Filament::getTenant()->id;
+                        $newPayment->candidate_id = $candidate + 1;
+                        $concepts = Candidate::find($candidate + 1)->concepts;
+                        $totalAmount = 0;
+                        foreach ($concepts as $concept) {
+                            if ($concept->type->value == 'exam' || $concept->type->value == 'module') {
+                                $totalAmount = $totalAmount + $concept->amount;
+                            }
+                            if ($concept->type->value == 'registration_fee' && Institute::find(Filament::getTenant()->id)->can_view_registration_fee == 1) {
+                                $totalAmount = $totalAmount - $concept->amount;
+                            }
+                        }
+                        $newPayment->amount = $totalAmount;
+                        $newPayment->status = 'pending';
+                        $newPayment->payment_method = 'financing by associated';
+                        $newPayment->payment_id = $data['payment_id'];
+                        $newPayment->currency = Country::find(Institute::find(Filament::getTenant()->id)->country)->monetary_unit;
+                        $newPayment->current_period = Carbon::now()->day(1);
+                        $newPayment->link_to_ticket = $data['link_to_ticket'];
+                        $newPayment->description = $data['description'];
+                        $newPayment->save();
+                    }
                 })
                 ->color(Color::hex('#0086b3')),
         ];
