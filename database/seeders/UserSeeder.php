be6ef89b20e704a19bf5ba4550ee867043189f74
Matias Yocca <matias@yocca.dev>
2024-03-18 00:22:28 +0000

    fix user name in seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -14,6 +14,7 @@ class UserSeeder extends Seeder
     public function run(): void
     {
         User::create([
+            'name' => 'Path Examinations',
             'email' => 'admin@pathexaminations.com',
             'password' => bcrypt('password'),
         ]);
