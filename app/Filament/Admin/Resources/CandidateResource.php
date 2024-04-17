970daeb06a1d9c3d0072b340e55b34f8a68fc77b
camilaarce <camilarce2710@gmail.com>
2024-04-17 21:03:13 +0000

    modificar cantidad de cuotas si no comenzo a pagar

--- app/Filament/Admin/Resources/CandidateResource.php
+++ app/Filament/Admin/Resources/CandidateResource.php
@@ -101,6 +101,15 @@ class CandidateResource extends Resource
                         } else {
                             $installmentsPaid = Payment::query()->where('candidate_id', $record->id)->where('status', 'approved')->count();
                         }
+                        if ($record->paymentStatus == 'unpaid') {
+                            $payment_deadline = $record
+                                ->exams
+                                ->min('payment_deadline');
+                            $state = round(
+                                now()->diffInMonths(Carbon::parse($payment_deadline), absolute: false),
+                                0,
+                            ) + 1;
+                        }
                         return $installmentsPaid . ' / ' . $state;
                     })
                     ->sortable(),
