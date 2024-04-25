52d2cc999e79a6ac05badb505218f94de3b89662
camilaarce <camilarce2710@gmail.com>
2024-04-25 18:51:09 +0000

    types of training

---
+++ app/Filament/Admin/Resources/TypeOfTrainingResource.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Filament\Admin\Resources;
+
+use App\Filament\Admin\Resources\TypeOfTrainingResource\Pages;
+use App\Filament\Admin\Resources\TypeOfTrainingResource\RelationManagers;
+use App\Models\TypeOfTraining;
+use Filament\Forms;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class TypeOfTrainingResource extends Resource
+{
+    protected static ?string $model = TypeOfTraining::class;
+
+    //protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    protected static ?string $navigationGroup = 'Settings';
+
+    protected static ?string $navigationLabel = 'Types of training';
+
+    protected static ?string $pluralModelLabel = 'Types of training';
+
+    protected static bool $hasTitleCaseModelLabel = false;
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                TextInput::make('name')
+                    ->required(),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('name')
+                    ->sortable(),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListTypeOfTrainings::route('/'),
+            'create' => Pages\CreateTypeOfTraining::route('/create'),
+            'edit' => Pages\EditTypeOfTraining::route('/{record}/edit'),
+        ];
+    }
+}
---
+++ app/Filament/Admin/Resources/TypeOfTrainingResource/Pages/CreateTypeOfTraining.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Filament\Admin\Resources\TypeOfTrainingResource\Pages;
+
+use App\Filament\Admin\Resources\TypeOfTrainingResource;
+use Filament\Actions;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateTypeOfTraining extends CreateRecord
+{
+    protected static string $resource = TypeOfTrainingResource::class;
+}
