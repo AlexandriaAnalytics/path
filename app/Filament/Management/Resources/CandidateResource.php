83ae01a8fcb88605932ccdad07675e6c48ea7d09
camilaarce <camilarce2710@gmail.com>
2024-04-25 17:07:55 +0000

    fix edit installments

--- app/Filament/Management/Resources/CandidateResource.php
+++ app/Filament/Management/Resources/CandidateResource.php
@@ -241,7 +241,7 @@ class CandidateResource extends Resource
                 Action::make('financing')
                     ->label('Installments')
                     ->icon('heroicon-o-document')
-                    ->visible(fn (Candidate $candidate) => $candidate->student->institute->installment_plans && Payment::query()->where('candidate_id', $candidate->id)->where('status', 'approved')->count() == 0)
+                    ->visible(fn (Candidate $candidate) => $candidate->student->institute->installment_plans && Payment::query()->where('candidate_id', $candidate->id)->where('status', 'approved')->count() == 0 && $candidate->paymentStatus == 'unpaid')
                     ->form([
                         TextInput::make('installments')
                             ->label('Number of installments')
