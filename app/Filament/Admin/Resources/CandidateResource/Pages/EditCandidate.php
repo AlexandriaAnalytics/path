f04f4e4f59d1b9dba4f743ab723244b2a96b87c0
camilaarce <camilarce2710@gmail.com>
2024-04-26 21:02:05 +0000

    borrar pagos softdeletes

--- app/Filament/Admin/Resources/CandidateResource/Pages/EditCandidate.php
+++ app/Filament/Admin/Resources/CandidateResource/Pages/EditCandidate.php
@@ -4,7 +4,9 @@ namespace App\Filament\Admin\Resources\CandidateResource\Pages;
 
 use App\Filament\Admin\Resources\CandidateResource;
 use App\Models\Concept;
+use App\Models\Payment;
 use App\Services\CandidateService;
+use Carbon\Carbon;
 use Filament\Actions;
 use Filament\Resources\Pages\EditRecord;
 use Filament\Support\Colors\Color;
@@ -24,5 +26,20 @@ class EditCandidate extends EditRecord
     {
         Concept::where('candidate_id', $this->record->id)->delete();
         CandidateService::createConcepts($this->record);
+    }
+
+
+
+    protected function mutateFormDataBeforeSave(array $data): array
+    {
+        $status = $data['status'];
+        if ($status == 'unpaid') {
+            $payment_deadline = $this->record->exams->min('payment_deadline');
+            $this->record->installments = round(now()->diffInMonths(Carbon::parse($payment_deadline), absolute: false), 0,) + 1;
+            $this->record->save();
+            Payment::query()->where('candidate_id', $this->record->id)->delete();
+        }
+
+        return $data;
     }
 }
