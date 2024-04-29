59c0578a6b1f6d63750202ba2e5350745b1ca5b3
jonas ojeda <jonojed98@gmail.com>
2024-04-29 17:11:57 -0300

    feat: Implementacion de recurso Performance en seccion Settings

---
+++ app/Filament/Admin/Resources/PerformanceResource.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Filament\Admin\Resources;
+
+use App\Filament\Admin\Resources\PerformanceResource\Pages;
+use App\Filament\Admin\Resources\PerformanceResource\RelationManagers;
+use App\Models\Performance;
+use Filament\Forms;
+use Filament\Forms\Components\Textarea;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class PerformanceResource extends Resource
+{
+    protected static ?string $model = Performance::class;
+    protected static ?string $navigationGroup = 'Settings';
+    protected static ?string $modelLabel = 'Performance';
+    protected static ?string $slug = 'performance';
+    // protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Textarea::make('answer')
+                    ->rows(10)
+                    ->cols(20),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('answer')
+                    ->searchable()
+                    ->sortable(),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ])
+            ->defaultSort('created_at', 'desc');
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListPerformances::route('/'),
+            'create' => Pages\CreatePerformance::route('/create'),
+            'edit' => Pages\EditPerformance::route('/{record}/edit'),
+        ];
+    }
+
+    public static function getPluralLabel(): string
+    {
+        return ('Performance');
+    }
+}
