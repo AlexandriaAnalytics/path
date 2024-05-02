2e57a80897c6a7c980392fa4ae3ade3a9864a4d0
camilaarce <camilarce2710@gmail.com>
2024-05-02 22:25:13 +0000

    prueba preference

--- app/Modules/Payments/MercadoPago/Controllers/MercadoPagoWebhookController.php
+++ app/Modules/Payments/MercadoPago/Controllers/MercadoPagoWebhookController.php
@@ -163,13 +163,13 @@ class MercadoPagoWebhookController extends Controller
 
         // Check if the candidate already has payments
         if (
-            $candidate->payments()
-            ->wherePaymentId($preapproval->id)
+            $candidate->payments
+            ->where('payment_id', $preapproval->id)
             ->count() != $preapprovalSummary->quotas
         ) {
             // Delete existing payments for this preapproval
-            $candidate->payments()
-                ->wherePaymentId($preapproval->id)
+            $candidate->payments
+                ->where('payment_id', $preapproval->id)
                 ->delete();
 
             $monthlyAmount = ($preapprovalSummary->charged_amount + $preapprovalSummary->pending_charge_amount) / ($preapprovalSummary->charged_quantity + $preapprovalSummary->pending_charge_quantity);
