853550d0d68112db149c5dbae0d93175ea331fe7
camilaarce <camilarce2710@gmail.com>
2024-05-02 18:42:37 +0000

    prueba preference

--- app/Modules/Payments/MercadoPago/Controllers/MercadoPagoWebhookController.php
+++ app/Modules/Payments/MercadoPago/Controllers/MercadoPagoWebhookController.php
@@ -175,12 +175,11 @@ class MercadoPagoWebhookController extends Controller
             $monthlyAmount = ($preapprovalSummary->charged_amount + $preapprovalSummary->pending_charge_amount) / ($preapprovalSummary->charged_quantity + $preapprovalSummary->pending_charge_quantity);
 
             $dateCreated = CarbonImmutable::parse($preapproval->date_created);
-            dd($preapprovalId);
             $payments = collect()->range(from: 1, to: $preapprovalSummary->quotas)
                 ->map(fn (int $installment) => [
                     'candidate_id' => $preapproval->external_reference,
                     'payment_method' => 'mercado_pago',
-                    'payment_id' => $preapprovalId,
+                    'payment_id' => $request->input('payment_id'),
                     'currency' => 'ARS',
                     'amount' => $monthlyAmount,
                     'installment_number' => $preapprovalSummary->quotas,
