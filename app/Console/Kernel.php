41346975d7a202da57a7ec8362b55e0b8b6e8d41
camilaarce <camilarce2710@gmail.com>
2024-05-04 17:44:42 +0000

    schedule

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,7 +13,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         // $schedule->command('inspire')->hourly();
-        $schedule->command('update-payments')->everyFifteenSeconds();
+        $schedule->command('update-payments')->daily();
     }
 
     /**
