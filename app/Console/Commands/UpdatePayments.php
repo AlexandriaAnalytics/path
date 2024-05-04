c7d098588e4894921692fd5ce5ffd4b403ff2a2f
camilaarce <camilarce2710@gmail.com>
2024-05-04 16:09:23 +0000

    schedule

---
+++ app/Console/Commands/UpdatePayments.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Payment;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Http;
+
+class UpdatePayments extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'update-payments';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        /* $payments = Payment::distinct()->pluck('payment_id');
+        foreach ($payments as $payment) { */
+        $payment = '1337';
+        $response = Http::withHeaders([
+            'Accept' => '*/*',
+            'User-Agent' => 'Thunder Client (https://www.thunderclient.com)',
+            'Authorization' => 'Bearer ' . config('mercadopago.access_token'),
+        ])
+            ->get('https://api.mercadopago.com/preapproval/' . $payment);
+        if (json_decode($response->body())->status == "authorized") {
+            Payment::where('payment_id', $payment)->where('current_installment', json_decode($response->body())->summarized->charged_quantity)->update(['status' => 'approved']);
+        }
+        /* } */
+
+        /* Payment::where('payment_id', '123')->where('current_installment', 1)->update(['suscription_code' => json_decode($response->body())->summarized->quotas]); */
+    }
+}
