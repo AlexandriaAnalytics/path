a7e21978b10476994b8cb32ab84cc817f8183c5b
camilaarce <camilarce2710@gmail.com>
2024-05-05 22:33:03 +0000

    fix trainee table

---
+++ app/Filament/Admin/Resources/TraineeResource.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Filament\Admin\Resources;
+
+use App\Filament\Admin\Resources\TraineeResource\Pages;
+use App\Filament\Admin\Resources\TraineeResource\RelationManagers;
+use App\Models\Trainee;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class TraineeResource extends Resource
+{
+    protected static ?string $model = Trainee::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                //
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                //
+            ])
+            ->filters([
+                Tables\Filters\TrashedFilter::make(),
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                    Tables\Actions\ForceDeleteBulkAction::make(),
+                    Tables\Actions\RestoreBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListTrainees::route('/'),
+            'create' => Pages\CreateTrainee::route('/create'),
+            'edit' => Pages\EditTrainee::route('/{record}/edit'),
+        ];
+    }
+
+    public static function getEloquentQuery(): Builder
+    {
+        return parent::getEloquentQuery()
+            ->withoutGlobalScopes([
+                SoftDeletingScope::class,
+            ]);
+    }
+}
---
+++ app/Filament/Admin/Resources/TraineeResource/Pages/CreateTrainee.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Filament\Admin\Resources\TraineeResource\Pages;
+
+use App\Filament\Admin\Resources\TraineeResource;
+use Filament\Actions;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateTrainee extends CreateRecord
+{
+    protected static string $resource = TraineeResource::class;
+}
