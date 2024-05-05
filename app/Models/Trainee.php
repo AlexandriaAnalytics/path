acc4b9e5b37ba88e508ba5c36a0e18f16ea8264b
camilaarce <camilarce2710@gmail.com>
2024-05-05 22:25:05 +0000

    model trainee

--- app/Models/Trainee.php
+++ app/Models/Trainee.php
@@ -4,8 +4,22 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Trainee extends Model
 {
     use HasFactory;
+    use SoftDeletes;
+
+    protected $fillable = ['full_name', 'phone', 'email', 'type_of_training_id', 'street_name', 'street_number', 'city', 'postcode', 'province_or_state', 'country_id', 'sections', 'files'];
+
+    public function typeOfTraining()
+    {
+        return $this->belongsTo(TypeOfTraining::class);
+    }
+
+    public function country()
+    {
+        return $this->belongsTo(Country::class);
+    }
 }
