06c0a9839a9f1429d803a727bea7fe5bca354c9d
camilaarce <camilarce2710@gmail.com>
2024-05-17 15:08:29 +0000

    files

---
+++ app/Filament/Trainee/Widgets/Files.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Filament\Trainee\Widgets;
+
+use App\Models\Trainee;
+use App\Models\User;
+use Filament\Widgets\StatsOverviewWidget as BaseWidget;
+use Filament\Widgets\StatsOverviewWidget\Stat;
+
+class Files extends BaseWidget
+{
+    protected function getStats(): array
+    {
+        return [
+            Stat::make('Files', Trainee::where('user_id', auth()->user()->id)->first()->files)
+                ->url(Trainee::where('user_id', auth()->user()->id)->first()->files, shouldOpenInNewTab: true)
+                ->extraAttributes([
+                    'style' => 'width: 70vw; overflow: hidden;
+                    white-space: nowrap; '
+                ])
+        ];
+    }
+}
