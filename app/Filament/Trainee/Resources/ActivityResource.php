3cb9ea38aafcdd16079ca7f317856308565843fc
camilaarce <camilarce2710@gmail.com>
2024-05-18 13:11:39 +0000

    solve activity

--- app/Filament/Trainee/Resources/ActivityResource.php
+++ app/Filament/Trainee/Resources/ActivityResource.php
@@ -11,15 +11,18 @@ use App\Models\Performance;
 use App\Models\Record;
 use App\Models\Section;
 use App\Models\Trainee;
+use Closure;
 use Filament\Forms;
 use Filament\Forms\Components\CheckboxList;
 use Filament\Forms\Components\FileUpload;
 use Filament\Forms\Components\MarkdownEditor;
+use Filament\Forms\Components\Radio;
 use Filament\Forms\Components\Section as ComponentsSection;
 use Filament\Forms\Components\TextInput;
 use Filament\Forms\Components\Wizard;
 use Filament\Forms\Components\Wizard\Step;
 use Filament\Forms\Form;
+use Filament\Forms\Set;
 use Filament\Infolists\Components\TextEntry;
 use Filament\Resources\Resource;
 use Filament\Tables;
@@ -99,10 +102,14 @@ class ActivityResource extends Resource
                                 $schema = [
                                     TextInput::make('question' . $index)
                                         ->default($questionModel->question)
+                                        ->disabled()
+                                        ->hiddenLabel()
                                         ->columnSpanFull(),
                                     TextInput::make('description' . $index)
                                         ->default($questionModel->description)
                                         ->hidden(fn () => empty($questionModel->description))
+                                        ->disabled()
+                                        ->hiddenLabel()
                                         ->columnSpanFull()
                                 ];
 
@@ -111,7 +118,8 @@ class ActivityResource extends Resource
                                     if (strpos($questionModel->multimedia, 'mp4') !== false) {
                                         $schema[] = MarkdownEditor::make('video' . $index)
                                             ->disabled()
-                                            ->default('<video width="320" height="240" controls>
+                                            ->hiddenLabel()
+                                            ->default('<video width="320" height="240" controls style="width: 100%;">
                                                           <source src="' . $multimediaUrl . '" type="video/mp4">
                                                           Your browser does not support the video tag.
                                                        </video>')
@@ -119,18 +127,40 @@ class ActivityResource extends Resource
                                     } else {
                                         $schema[] = MarkdownEditor::make('image' . $index)
                                             ->disabled()
+                                            ->hiddenLabel()
                                             ->default('<img src="' . $multimediaUrl . '" alt="Multimedia" style="max-width: 100%; height: auto;">')
                                             ->columnSpanFull();
                                     }
                                 }
 
-                                $schema[] = CheckboxList::make('answers' . $index)
-                                    ->options($questionModel->aswers)
-                                    ->disabled(fn () => $index == 0);
 
-                                $schema[] = CheckboxList::make('answers' . $index)
-                                    ->options($questionModel->performance)
-                                    ->disabled();
+                                $schema[] = Radio::make('answers' . $index)
+                                    ->options($questionModel->aswers)
+                                    ->label('Answers')
+                                    ->disabled(fn () => $index == 0)
+                                    ->reactive()
+                                    ->afterStateUpdated(function ($set, $state) use ($index, $questionModel) {
+                                        $performanceId = $questionModel->performance[$state] ?? null;
+                                        if ($performanceId) {
+                                            $performance = Performance::find($performanceId);
+                                            if ($performance) {
+                                                $set('performance' . $index, $performance->answer);
+                                            }
+                                        }
+                                    });
+
+                                if ($index == 0) {
+                                    $schema[] = CheckboxList::make('answers' . $index)
+                                        ->label('Performance')
+                                        ->options($questionModel->performance)
+                                        ->disabled();
+                                } else {
+                                    $schema[] = TextInput::make('performance' . $index)
+                                        ->disabled()
+                                        ->label('Performance')
+                                        ->hidden(fn () => $index == count($examinerActivity->questions) - 1)
+                                        ->default('Choose an answer');
+                                }
                             }
                             $steps[] = Step::make('Question ' . ($index + 1))
                                 ->schema($schema)->columns(2);
@@ -139,7 +169,11 @@ class ActivityResource extends Resource
                             Wizard::make($steps)
                         ];
                     })
-
+                    ->action(function (array $data, Record $record) {
+                        $examinerActivity = ExaminerActivity::where('section_id', $record->section_id)->first();
+                        $record->performance_id = ExaminerQuestion::find($examinerActivity->questions[count($examinerActivity->questions) - 1])->performance[$data['answers' . count($examinerActivity->questions) - 1]];
+                        $record->save();
+                    })
             ]);
     }
 
