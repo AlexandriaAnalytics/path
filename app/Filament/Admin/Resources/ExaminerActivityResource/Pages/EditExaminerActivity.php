6364a57ef2a24fd29d5866779f3639db4dde588e
camilaarce <camilarce2710@gmail.com>
2024-05-19 21:04:17 +0000

    fix edit activity

--- app/Filament/Admin/Resources/ExaminerActivityResource/Pages/EditExaminerActivity.php
+++ app/Filament/Admin/Resources/ExaminerActivityResource/Pages/EditExaminerActivity.php
@@ -4,6 +4,7 @@ namespace App\Filament\Admin\Resources\ExaminerActivityResource\Pages;
 
 use App\Filament\Admin\Resources\ExaminerActivityResource;
 use App\Models\ExaminerActivity;
+use App\Models\ExaminerQuestion;
 use Filament\Actions;
 use Filament\Resources\Pages\EditRecord;
 
@@ -31,6 +32,30 @@ class EditExaminerActivity extends EditRecord
             }
         }
 
+        return $data;
+    }
+
+    protected function mutateFormDataBeforeSave(array $data): array
+    {
+        $questions = [];
+        foreach ($data['questions'] as $question) {
+            $examinerQuestion = new ExaminerQuestion();
+            $examinerQuestion->question = $question['question'];
+            $examinerQuestion->description = $question['description'] ? $question['description'] : null;
+            $examinerQuestion->open_or_close = $question['open_or_close'];
+            $aswers = [];
+            $performances = [];
+            foreach ($question['aswers'] as $answer) {
+                $aswers[] = $answer['aswer'];
+                $performances[] = $answer['performance'];
+            }
+            $examinerQuestion->aswers = $aswers;
+            $examinerQuestion->performance = $performances;
+            $examinerQuestion->multimedia = $question['multimedia'];
+            $examinerQuestion->save();
+            $questions[] = $examinerQuestion->id;
+        }
+        $data['questions'] = $questions;
         return $data;
     }
 }
