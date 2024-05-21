e7bcc6521ee25b08508409a80e16cff493cd02e7
camilaarce <camilarce2710@gmail.com>
2024-05-21 15:32:46 +0000

    open text

--- app/Models/MultipleChoice.php
+++ app/Models/MultipleChoice.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-class Multiple_choice extends Model
+class MultipleChoice extends Model
 {
     use HasFactory, SoftDeletes;
 
--- app/Models/TrueFalse.php
+++ app/Models/TrueFalse.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-class True_false extends Model
+class TrueFalse extends Model
 {
     use HasFactory, SoftDeletes;
 
