36acae216f88e087f4bb66cda90ae68052c87fc4
camilaarce <camilarce2710@gmail.com>
2024-05-21 15:27:25 +0000

    true or false

---
+++ app/Models/Multiple_choice.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Multiple_choice extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = ['answers', 'correct', 'comments', 'question_id'];
+
+    protected $casts = [
+        'answers' => 'array',
+        'correct' => 'array',
+        'comments' => 'array'
+    ];
+}
