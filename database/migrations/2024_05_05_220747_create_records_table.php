8fc87662d8073f42219b7d79db612ff99e67acb8
Camila Arce <camila.arce@gpsde.local>
2024-05-31 10:04:48 -0300

    activity without question

--- database/migrations/2024_05_05_220747_create_records_table.php
+++ database/migrations/2024_05_05_220747_create_records_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('records', function (Blueprint $table) {
             $table->id();
             $table->foreignId('trainee_id')->constrained();
-            $table->foreignId('section_id')->constrained()->references('id')->on('levels');
+            $table->foreignId('section_id')->constrained()->references('id')->on('sections');
             $table->foreignId('status_activity_id')->constrained();
             $table->string('comments')->nullable();
             $table->timestamps();
