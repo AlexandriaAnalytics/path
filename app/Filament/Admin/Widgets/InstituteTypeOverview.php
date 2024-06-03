4b36c2b99ca576078b1a15ab200ef82652f13068
camilaarce <camilarce2710@gmail.com>
2024-06-03 15:35:14 +0000

    widgets admin

--- app/Filament/Admin/Widgets/InstituteTypeOverview.php
+++ app/Filament/Admin/Widgets/InstituteTypeOverview.php
@@ -8,6 +8,7 @@ use Filament\Widgets\StatsOverviewWidget as BaseWidget;
 use Filament\Widgets\StatsOverviewWidget\Stat;
 use App\Models\Institute;
 use App\Models\Student;
+use App\Models\User;
 
 class InstituteTypeOverview extends BaseWidget
 {
@@ -17,7 +18,8 @@ class InstituteTypeOverview extends BaseWidget
             Stat::make('Institutes', Institute::all()->count()),
             Stat::make('Exams', Exam::all()->count()),
             Stat::make('Students', Student::all()->count()),
-            Stat::make('Pending changes', Change::where('status', 0)->count())
+            Stat::make('Pending changes', Change::where('status', 0)->count()),
+            Stat::make('Users', User::all()->count())
         ];
     }
 }
