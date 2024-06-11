384bdfa86b02cce85fbe07ea2caca5257918a937
camilaarce <camilarce2710@gmail.com>
2024-06-11 13:37:21 +0000

    currency in institutes

--- app/Models/Institute.php
+++ app/Models/Institute.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
@@ -140,6 +141,15 @@ class Institute extends Model
 
     public function getCurrencyAttribute(){
         return Country::find($this->country)->monetary_unit;
+    }
+
+    public function currency(): Attribute
+    {
+        return Attribute::make(
+            get: function () {
+                return Country::find($this->country)->monetary_unit;
+            },
+        );
     }
 
     public function financings() {
