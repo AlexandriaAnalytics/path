46031ec343b0f630a9a6b674ad169316ef883cdc
camilaarce <camilarce2710@gmail.com>
2024-07-02 15:30:07 +0000

    no permitir ventana emergente

--- resources/views/filament/iframes.blade.php
+++ resources/views/filament/iframes.blade.php
@@ -1,4 +1,4 @@
 
 <body>
-    <iframe src="<?php echo $url ?>" width="100%" height="500" frameborder="0"></iframe>
+    <iframe src="<?php echo $url ?>" width="100%" height="500" frameborder="0" sandbox="allow-scripts allow-same-origin allow-forms allow-popups=false"></iframe>
 </body>
